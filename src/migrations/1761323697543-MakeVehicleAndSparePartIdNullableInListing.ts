535622e5da531defb015366d4ecb8566d5424a63
Bassel Halabi <bassel@shipbee.io>
2025-10-24 19:38:24 +0300

    fix migration

--- src/migrations/1761323697543-MakeVehicleAndSparePartIdNullableInListing.ts
+++ src/migrations/1761323697543-MakeVehicleAndSparePartIdNullableInListing.ts
@@ -3,6 +3,8 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class MakeVehicleAndSparePartIdNullableInListing1761323697543 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        //add sparePartId to listings table and make both vehicleId and sparePartId nullable
+        await queryRunner.query(`ALTER TABLE "listings" ADD COLUMN "sparePartId" uuid REFERENCES "spare_parts"(id) ON DELETE CASCADE`);
         await queryRunner.query(`ALTER TABLE "listings" ALTER COLUMN "vehicleId" DROP NOT NULL`);
         await queryRunner.query(`ALTER TABLE "listings" ALTER COLUMN "sparePartId" DROP NOT NULL`);
     }
@@ -10,6 +12,7 @@ export class MakeVehicleAndSparePartIdNullableInListing1761323697543 implements 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "listings" ALTER COLUMN "vehicleId" SET NOT NULL`);
         await queryRunner.query(`ALTER TABLE "listings" ALTER COLUMN "sparePartId" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "listings" DROP COLUMN "sparePartId"`);
     }
 
 }
